ce22d3e1d9786f339b760404757e9d3fb06fc7b5
badhon <bdalbot116@gmail.com>
2024-01-15 15:44:41 +0600

    Header, Hero section, Students table and footer copied from tamplate

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import StudentsTable from "./components/StudentsTable";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <StudentsTable />
+      <Footer />
+    </>
+  );
+}
